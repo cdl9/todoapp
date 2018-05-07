39ce71583c897e6f2e24a33b8e212801bf50217e
cdl9 <cdl9@njit.edu>
2018-05-06 22:07:12 -0400

    First commit

---
+++ src/components/taskbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class TaskBar extends React.Component{
+	
+	render(){
+		return(
+			<div>
+				<input type="text" ref="task" placeholder="add your tasks here"/>
+			</div>
+		);
+
+	}
+
+}
